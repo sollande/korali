6b8faa189db60a18bef6c2b002dc3370676aedfb
pollakg <pollakg@daint104.login.cscs.ch>
2022-04-19 11:13:09 +0200

    Adds function to return newst dir from date dirs.
    - i.e. to get the most recent dir from a folder of directories.

--- examples/learning/supervised/Cylinder/utilities.py
+++ examples/learning/supervised/Cylinder/utilities.py
@@ -275,6 +275,18 @@ def make_parser():
     )
     return parser
 
+
+def get_newest_dir(dest):
+    """Returns the newst dir for a folder of dirs consisting of dates
+    :param dest: folder of dires in with dates as names
+    :returns: path
+
+    """
+    files = os.listdir(dest)
+    dates = [datetime.strptime(f) for f in files]
+    indices = [i[0] for i in sorted(enumerate(dates), key=lambda x:x[1])]
+    return files[indices[-1]]
+
 def move_dir(src, dest):
     """Move all files from one directory to another
 
