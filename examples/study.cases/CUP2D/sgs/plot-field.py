9072209f66dbf47463da53c8a6dba3b9f70b409a
wadaniel <wadaniel@ethz.ch>
2022-04-29 23:02:28 +0200

    plotting

--- examples/study.cases/CUP2D/sgs/plot-field.py
+++ examples/study.cases/CUP2D/sgs/plot-field.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 ####### Parsing arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--filename', help='NPZ file with flow field.', required=True, type=str)
+parser.add_argument('--output', help='Name of plot.', required=False, type=str, default='field.png')
 parser.add_argument('--nfilter', help='Freq filter.', required=True, type=int)
 parser.add_argument('--L', help='Length of field.', required=False, type=float, default=2.*np.pi)
 args = parser.parse_args()
@@ -15,9 +16,6 @@ data = np.load(args.filename)
 
 u = data['u']
 v = data['v']
-
-print(u.shape)
-print(v.shape)
 
 N = u.shape[0]
 
@@ -35,30 +33,20 @@ curl = dvdx - dudy
 Fu = np.fft.fft2(u)
 Fv = np.fft.fft2(v)
 
-print(Fu.shape)
-print(Fv.shape)
-
 FreqCompRows = np.fft.fftfreq(Fu.shape[0], L / (2*np.pi*N))
 FreqCompCols = np.fft.fftfreq(Fu.shape[1], L / (2*np.pi*N))
-print(FreqCompRows)
-print(FreqCompCols)
 
-idxH = np.abs(FreqCompCols) <= Nf//2
-
-print(idxH)
+idxH = np.abs(FreqCompCols) > Nf//2
 idxH2d = np.ix_(idxH, idxH)
 
-Fuh = Fu[idxH2d] * (Nf / N)**2
-Fvh = Fv[idxH2d] * (Nf / N)**2
+Fuh = Fu
+Fvh = Fv
 
-print(Fuh.shape)
-print(Fvh.shape)
+Fuh[idxH2d] = 0
+Fvh[idxH2d] = 0
 
 uh = np.real(np.fft.ifft2(Fuh))
 vh = np.real(np.fft.ifft2(Fvh))
-
-print(uh.shape)
-print(vh.shape)
 
 dxh = L/(Nf+1)
 dyh = L/(Nf+1)
@@ -80,8 +68,6 @@ umax = max(u.max(), v.max())
 umin = min(u.min(), v.min())
 ulevels = np.linspace(umin, umax, 20)
 
-fig1Name="field.png"
-
 fig1, axs1 = plt.subplots(2, 3, sharex='col', sharey='col', figsize=(10,10))
 axs1[0,0].contourf(X, Y, u, ulevels)
 axs1[0,0].set_aspect('equal', adjustable='box')
@@ -95,7 +81,7 @@ axs1[0,2].set_aspect('equal', adjustable='box')
 xaxish = np.arange(0,L,dxh)
 yaxish = np.arange(0,L,dxh)
 
-Xh, Yh = np.meshgrid(xaxish, yaxish, indexing='ij')
+Xh, Yh = np.meshgrid(xaxis, yaxis, indexing='ij')
 
 axs1[1,0].contourf(Xh, Yh, uh, ulevels)
 axs1[1,0].set_aspect('equal', adjustable='box')
@@ -106,7 +92,6 @@ axs1[1,1].set_aspect('equal', adjustable='box')
 axs1[1,2].contourf(Xh, Yh, curlh) #, ulevels)
 axs1[1,2].set_aspect('equal', adjustable='box')
 
-
-fig1.savefig(fig1Name)
+fig1.savefig(args.output)
 
 
