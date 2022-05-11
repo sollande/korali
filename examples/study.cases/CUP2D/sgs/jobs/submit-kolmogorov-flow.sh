f91ca0eb3b9d34327fd857556cc4b9cba3334dd7
Daniel Waelchli <wadaniel@ethz.ch>
2022-05-11 16:04:00 +0200

    setup

--- examples/study.cases/CUP2D/sgs/jobs/submit-kolmogorov-flow.sh
+++ examples/study.cases/CUP2D/sgs/jobs/submit-kolmogorov-flow.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 RUN=0
-NGRID=128
+#GRIDS=( 128 )
+GRIDS=( 256 512 1024 2048 )
 
-RUNNAME="kolmogorov_flow_${RUN}_${NGRID}"
+for NGRID in "${GRIDS[@]}"
+do
+    RUNNAME="kolmogorov_flow_${RUN}_${NGRID}"
+    echo Launching ${RUNNAME}
 
-mkdir -p ./runs/
-launchname="${0##*/}"
-cp $launchname "./runs/${RUNNAME}.sh"
-git diff > "./runs/gitdiff_${RUNNAME}.txt"
+    mkdir -p ./runs/
+    launchname="${0##*/}"
+    cp $launchname "./runs/${RUNNAME}.sh"
+    git diff > "./runs/gitdiff_${RUNNAME}.txt"
 
-module purge
-module load daint-gpu gcc GSL/2.7-CrayGNU-21.09 cray-hdf5-parallel cray-python cdt-cuda craype-accel-nvidia60
+    module purge
+    module load daint-gpu gcc GSL/2.7-CrayGNU-21.09 cray-hdf5-parallel cray-python cdt-cuda craype-accel-nvidia60
 
-./sbatch-run-kolmogorov-flow.sh $RUNNAME $NGRID
+    ./sbatch-run-kolmogorov-flow.sh $RUNNAME $NGRID
+
+done
